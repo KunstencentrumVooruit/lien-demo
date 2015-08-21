34eb09a1c77f5fabb46330a9f47ade4ce65a7ee7
Martijn Kerckhaert <martijn_kerckhaert@hotmail.com>
2015-08-21 15:21:54 +0200

    Forgive me father. I have not commited in a long time. I only come with good news though, I fixed a lot of issues and this seems to be a stable version. Please forgive me for not pushing the progress.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import locale
 import datetime
 import datetime
 from dateutil import parser
+import js2py
+
+global_teller = 1
 
 def getDate(delta):
   today = datetime.date.today() + datetime.timedelta(days=delta)
@@ -23,73 +26,166 @@ class googleSheet():
 
 
 def getActivity(query):
-  # data = query.get_data("/event/5692949505-1416475956")
-  data = query.get_data("/events/name%3Ademo")
-
-  #CONCERTZAAL
-  #BALZAAL
-  #THEATERZAAL
-
+  data = query.get_data("/events/event%3Aprofile%3A%22demo(c)%22%20event%3Adate%3A%23next50days%20(event%3Alocation%3Aconcertzaal%20%2B%20event%3Alocation%3Atheaterzaal%20%2B%20event%3Alocation%3Abalzaal%20%2B%20event%3Alocation%3Acafe)")
   i=0
-
-  # voorstellingen = []
-  # wut = []
-  samen = []
+  samen = {}
 
   for w in data["data"]:
-    id=data["data"][i]["id"]
+    ide=data["data"][i]["id"]
     lokatie = data['data'][i]['locations'][0]['name']
     aanvangsuur = data['data'][i]['starttime']
+    artiest = data['data'][i]['name']
+    status = data['data'][i]['status']['name']
 
-    # aanvangsuurr = datetime.strptime(aanvangsuur, '%Y-%m-%dT%H:%M:%S.%fZ')
-    aanvangsuurr = parser.parse(aanvangsuur)
+    samen[aanvangsuur] = [lokatie, artiest, status, ide]
 
-
-
-    # aanvangsuur=data["data"][i]["defaultschedulestarttime"]
-
-    samen.append(lokatie)
-    if aanvangsuur > date.today()-datetime.timedelta(days=7):
-      samen.append(aanvangsuurr)
-
-    # wut.append(aanvangsuur)
     i=i+1
 
   return samen
 
 
+
 def exportToSheet(query, worksheet, samen):
 
-  # worksheet.update_cell(5, 5, naam)
-  teller = 0
+  cellOffset = 2
+  global global_teller
 
-  for sam in samen:
-    print sam
-    # print wut
-    # worksheet.update_cell(1, teller, voorstelling[teller])
+  for key in sorted(samen):
 
-  teller = teller + 1
+    aanvangsuur = key
+    lokatie = samen[key][0]
+    artiest = samen[key][1]
+    status = samen[key][2]
+    ide = samen[key][3]
+
+    #string van maken
+    aanvangsuur = str(aanvangsuur)
+    #Heel de GMT boel eraf cutten
+    aanvangsuur = aanvangsuur[:-9]
+    #De T eruit halen en vervangen door spatie
+    aanvangsuur = aanvangsuur.replace("T", " ")
+
+    #Positioning
+    if lokatie == "CONCERTZAAL":
+      worksheet.update_cell(cellOffset, 1, aanvangsuur)
+      worksheet.update_cell(cellOffset, 3, artiest)
+      worksheet.update_cell(cellOffset, 4, status)
+      worksheet.update_cell(cellOffset, 13, ide)
+      cellOffset=cellOffset+1
+      global_teller += 1
+
+    if lokatie == "BALZAAL":
+      worksheet.update_cell(cellOffset, 5, aanvangsuur)
+      worksheet.update_cell(cellOffset, 7, artiest)
+      worksheet.update_cell(cellOffset, 8, status)
+      worksheet.update_cell(cellOffset, 13, ide)
+      cellOffset=cellOffset+1
+      global_teller += 1
+
+    if lokatie == "THEATERZAAL":
+      worksheet.update_cell(cellOffset, 9, aanvangsuur)
+      worksheet.update_cell(cellOffset, 11, artiest)
+      worksheet.update_cell(cellOffset, 12, status)
+      worksheet.update_cell(cellOffset, 13, ide)
+      cellOffset=cellOffset+1
+      global_teller += 1
+
+  #Make the headers
+  worksheet.update_cell(1,1, "CONCERTZAAL")
+  worksheet.update_cell(1,2, "Opmerking")
+  worksheet.update_cell(1,3, "Artist")
+  worksheet.update_cell(1,4, "Status")
+
+  worksheet.update_cell(1,5, "BALZAAL")
+  worksheet.update_cell(1,6, "Opmerking")
+  worksheet.update_cell(1,7, "Artist")
+  worksheet.update_cell(1,8, "Status")
+
+  worksheet.update_cell(1,9, "THEATERZAAL")
+  worksheet.update_cell(1,10, "Opmerking")
+  worksheet.update_cell(1,11, "Artist")
+  worksheet.update_cell(1,12, "Status")
+  worksheet.update_cell(1,13, "Event ID")
 
 
 def main():
+  #Select sheet
   gSheet = googleSheet('test')
 
+  #Select query
   query = yesplanAPIQuery2()
 
+  #Select worksheet of above assigned sheet
   worksheet = gSheet.wks.get_worksheet(0)
 
-  cell_list = worksheet.range('A4:B30')
-  for cell in cell_list:
-    cell.value=''
+  #Store all comments
+  amountofrows = worksheet.row_count
+  all_ids = worksheet.col_values(13)
+  concertzaal_comments = worksheet.col_values(2)
+  balzaal_comments = worksheet.col_values(6)
+  theaterzaal_comments = worksheet.col_values(10)
 
-  worksheet.update_cells(cell_list)
+  # print all_ids
+  # print concertzaal_comments
+  # print balzaal_comments
+  # print theaterzaal_comments
+
+  savecomments = {}
+  tellen = 0
+  lengte = len(all_ids)
+
+  #Here we pair the correct comments to the correct IDs
+  for _ in range(lengte):
+    if concertzaal_comments[tellen] != None:
+      savecomments[all_ids[tellen]] = [concertzaal_comments[tellen]]
+      tellen += 1
+    elif balzaal_comments[tellen] != None:
+      savecomments[all_ids[tellen]] = [balzaal_comments[tellen]]
+      tellen += 1
+    elif theaterzaal_comments[tellen] != None:
+      savecomments[all_ids[tellen]] = [theaterzaal_comments[tellen]]
+      tellen += 1
+    else:
+      tellen += 1
+
+  #clearing  worksheet because else it will keep the old comments on wrong places
+  cells = worksheet.range('A1:N'+str(amountofrows))
+  for cell in cells:
+      cell.value=''
+  worksheet.update_cells(cells)
+
+  #Get activities
   naam = getActivity(query)
+  #Post activities
   exportToSheet(query, worksheet, naam)
 
+  #Set lenght of sheet, this way we can resize it correctly and when we get the comments out
+  #we wont have to loop over hundreds of empty cells.
+  amountofrows = worksheet.row_count
+  worksheet.resize(global_teller)
+  #Add another row because else the comment arrays wont have the same length
+  #and then I wont be able to match  them to ID
+  endrow = ["Do not remove ", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove", "Do not remove"]
+  worksheet.append_row(endrow)
 
+  # Here we check where the comment has to be placed plus actually place them.
+  for key in savecomments:
 
-
-
+    comment = savecomments[key][0]
+    #I have to do try/except because when you remove an event that had a comment
+    #it will try to find and throw exception that the corresponding ID
+    #is not in the new list grabbed from API
+    try:
+      idplaats = worksheet.find(key)
+    except Exception:
+      continue
+        #Checking where the comment should be placed.
+    if worksheet.cell(idplaats.row, 1).value != '' and idplaats != "No":
+      worksheet.update_cell(idplaats.row, 2, comment)
+    elif worksheet.cell(idplaats.row, 5).value != '' and idplaats != "No":
+      worksheet.update_cell(idplaats.row, 6, comment)
+    elif worksheet.cell(idplaats.row, 9).value != '' and idplaats != "No":
+      worksheet.update_cell(idplaats.row, 10, comment)
 
 main()
 
